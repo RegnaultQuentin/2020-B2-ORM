954c594674d3e6da2d468db5963d74ee94dd247e
Quentin Regnault <quentin.regnault@ynov.com>
2019-10-23 11:16:04 +0200

    Ex1 et 2

--- tp1/1/index.js
+++ tp1/1/index.js
@@ -7,7 +7,30 @@ exports = module.exports = {
   datasets,
   algo: function (input) {
     // YOUR CODE BETWEEN HERE
-
+    
+    // console.log(input.length)
+    // Récupération de la place d'arriver
+    let result = 0
+    for (let a = 1; a < input.length; ++a){
+      
+      
+      // console.log(input[0] + input[a])
+      // Conditions pour le résultat 
+      placearrive = (input[0] += parseInt(input[a].split(' ')[0])) // <=== J'ai une question la pourquoi je peux pas le mettre sur une ligne avec un + entre les deux ? 
+      placearrive = (input[0] -= parseInt(input[a].split(' ')[1])) // <===
+      
+      if (placearrive <= 100) {
+      
+        result = 1000
+      }
+      else if (placearrive > 100 && placearrive <= 10000){
+        result = 100
+      }
+      else {
+        result = "KO"
+      }
+    }
+    return result
     // AND HERE
   },
   verify: function (dataset, output) {
--- tp1/2/index.js
+++ tp1/2/index.js
@@ -8,6 +8,26 @@ exports = module.exports = {
   algo: function (input) {
     // YOUR CODE BETWEEN HERE
 
+    // récuper le nombre de cartons
+    nbcartons = input[0]
+    // console.log(input)
+    nballerretour = 1
+
+    for (let a = 1; a < input.length; ++a){
+      //calcul du poids de tous les cartons
+      poidcartons = nbcartons * input[a]
+      
+      if (poidcartons <= 100){
+        nballerretour = 1
+      }
+      // il faudrait incrémenter +1 à chaque fois mais j'ai pas eu le temps de trouver 
+      else if(poidcartons > 100){
+        nballerretour += 1
+      }
+
+    }
+    return nballerretour
+    // console.log(poidcartons)
     // AND HERE
   },
   verify: function (dataset, output) {
